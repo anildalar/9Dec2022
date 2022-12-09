433483ce94b1808f92806834aca72505cd00a9e1
unknown <dollorinfotech@gmail.com>
2022-12-09 09:06:54 +0530

    commit 12

---
+++ js/script2.js
@@ -0,0 +1,16 @@
+console.log("hi 'anil'");
+console.log('Hi "ravi"');
+console.log(`Hi 'Rakesh'`);
+console.log(`Hi "Anjali"`);
+
+console.log('Hi \'Harish\' ');
+
+console.log("Hi \"Pankaj\"");
+
+console.log("/\\/\\/\\");
+
+
+
+
+
+
